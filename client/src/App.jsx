d186da3131f9666328b72b9a34afb6cf73dfa522
ccchen <chencc1213@gmail.com>
2025-05-10 22:35:35 +0900

    feat: home page done. fixed the toast problem.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
-import Toaster from 'react-hot-toast'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes('seller')
@@ -12,7 +13,7 @@ const App = () => {
       {isSellerPath ? null : <Navbar />}
 
       <Toaster />
-      
+
       <div
         className={`${isSellerPath ? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}
       >
@@ -20,6 +21,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+
+      {!isSellerPath && <Footer />}
     </div>
   )
 }
