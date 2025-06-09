0b050336afdfb0612f2e20d905fd5c303c4bdb2d
ccchen <chencc1213@gmail.com>
2025-06-09 23:09:24 +0900

    feat: Debug is-auth request. Add seller controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -104,13 +104,13 @@ export const login = async (req, res) => {
 // Check Auth: /api/user/is-auth
 export const isAuth = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req;
     const user = await User.findById(userId).select('-password');
     return res.json({  success: true, user })
 
   } catch (error) {
     console.log(error.message);
-    res.json({ success: false, message: error.message })
+    res.json({ success: false, message: 'isAuth: '+ error.message })
   }
 }
 
--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -9,14 +9,14 @@ const authUser = async (req, res, next) => {
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
     if(tokenDecode.id) {
-      req.body.userId = tokenDecode.id;
+      req.userId = tokenDecode.id;
     } else {
       return res.json({ success: false, message: 'Not Authorized' });
     }
     next();
 
   } catch (error) {
-    return res.json({ success: false, message: error.message });
+    return res.json({ success: false, message: 'authUser'+ error.message });
   }
 }
 
