69aad204f08f7508a30c903c586a0a3a81e3185b
ccchen <chencc1213@gmail.com>
2025-06-16 23:29:18 +0900

    fix: import errors

---
+++ server/controllers/productController.js
@@ -0,0 +1,37 @@
+
+// Add Product: /api/product/add
+export const addProduct = async (req, res) => { 
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+
+// Get Product List: /api/product/list
+export const productList = async (req, res) => { 
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+// Get Single Product: /api/product/id
+export const productById = async (req, res) => { 
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+// Change Product inStock: /api/product/stock
+export const changeStock = async (req, res) => { 
+  try {
+    
+  } catch (error) {
+    
+  }
+}
